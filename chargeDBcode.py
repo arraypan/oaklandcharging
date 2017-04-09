0bea795d518f736f5c0d6ebc63a07cdfcd2d89c2
Dmadurs <djmadurs@oakland.edu>
2017-04-09 13:02:25 -0400

    Add files via upload
    Updated Database code. Included additional MySQL queries

--- chargeDBcode.py
+++ chargeDBcode.py
@@ -20,6 +20,61 @@ def showInfo():
                 cursor.close()
                 conn.close()
 
+def showAdmin():
+        try:
+                dbconfig = read_db_config()
+                conn = MySQLConnection(**dbconfig)
+                cursor = conn.cursor()
+                cursor.execute("SELECT * FROM admin")
+                rows = cursor.fetchall()
+
+                print('Total Entries:', cursor.rowcount)
+                for row in rows:
+                    print(row)
+
+        except Error as e:
+                print(e)
+        finally:
+                cursor.close()
+                conn.close()
+
+def showBoth():
+        try:
+                dbconfig = read_db_config()
+                conn = MySQLConnection(**dbconfig)
+                cursor = conn.cursor()
+                cursor.execute("SELECT admin.GID, admin.StudentName, Device, Email, TheDate, TheTime FROM admin join info on admin.GID = info.GID")
+                rows = cursor.fetchall()
+
+                print('Total Entries:', cursor.rowcount)
+                for row in rows:
+                    print(row)
+
+        except Error as e:
+                print(e)
+        finally:
+                cursor.close()
+                conn.close()
+
+def showSpecificBoth(gid):
+        try:
+
+                dbconfig = read_db_config()
+                conn = MySQLConnection(**dbconfig)
+                cursor = conn.cursor()
+                query = ("SELECT admin.GID, admin.StudentName, Device, Email, TheDate, TheTime FROM admin join info on admin.GID = info.GID where admin.GID = %s")
+                cursor.execute(query % gid)
+                rows = cursor.fetchall()
+
+                for row in rows:
+                   print(row)
+
+        except Error as e:
+                print(e)
+        finally:
+                cursor.close()
+                conn.close()
+
 def insert_info(gid, name, device, email):
         query = "insert into info values(%s, %s, %s, %s)"
         args = (gid, name, device, email)
@@ -39,9 +94,9 @@ def insert_info(gid, name, device, email):
                 cursor.close()
                 conn.close()
 
-def insert_admin(gid, name, dateTime):
-        query = "insert into admin values(%s, %s, %s)"
-        args = (gid, name, dateTime)
+def insert_admin(gid, name, date, time):
+        query = "insert into admin values(%s, %s, %s, %s)"
+        args = (gid, name, date, time)
 
         try:
                 db_config = read_db_config()
@@ -58,8 +113,26 @@ def insert_admin(gid, name, dateTime):
                 cursor.close()
                 conn.close()
 
-def deleteEntry(gid):
+def deleteAdminEntry(gid):
         query = "delete from admin where GID = %s"
+        
+        try:
+                db_config = read_db_config()
+                conn = MySQLConnection(**db_config)
+ 
+                cursor = conn.cursor()
+                cursor.execute(query, (gid,))
+                conn.commit()
+
+        except Error as e:
+                print(e)
+ 
+        finally:
+                cursor.close()
+                conn.close()
+
+def deleteInfoEntry(gid):
+        query = "delete from info where GID = %s"
         
         try:
                 db_config = read_db_config()
@@ -77,6 +150,7 @@ def deleteEntry(gid):
                 conn.close()
         
 if __name__ == '__main__':
-        #insert_admin('00216674', 'David Madurski', '2017-07-26 12:30:00')
+        #insert_admin('00216674', 'David Madurski', '2017-07-26', '12:30:00')
         #insert_info('00216674', 'David Madurski', 'Motorola X', 'djmadurs@oakland.edu')
-        #deleteEntry('00216674')
+        showSpecificBoth('00216674')
+        
